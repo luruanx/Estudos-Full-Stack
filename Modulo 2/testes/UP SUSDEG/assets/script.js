e1ebdd494954614ddb8e73e85149f4436d536f82
Ruan Richard <Richard_10luan@hotmail.com>
2024-03-14 23:44:57 -0300

    slider-article

--- Modulo 2/testes/UP SUSDEG/assets/script.js
+++ Modulo 2/testes/UP SUSDEG/assets/script.js
@@ -47,16 +47,22 @@ function plusSlides(n) {
     showSlides(slideIndex += n);
 }
 
-function currentSlides(n) {
+function currentSlide(n) {
     showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("slide-box");
+    var slides = document.getElementsByClassName("slides");
+    var dots = document.getElementsByClassName("dot");
     if (n > slides.length) { slideIndex = 1 }
-    if ( n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
+    if (n < 1) { slideIndex = slides.length }
+    for ( i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace("active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
