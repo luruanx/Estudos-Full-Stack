7bbdd687c1f6382732c317dcfed9ac31385afaeb
Ruan Richard <Richard_10luan@hotmail.com>
2024-05-06 08:38:48 -0300

    repetição while and do

--- JS/Modulo E/ex01/exe001.js
+++ JS/Modulo E/ex01/exe001.js
@@ -1,5 +1,16 @@
+/* Repetição com Teste Lógico Início
+
 var c = 1
 while (c <= 6) {
     console.log(`Passo ${c}`)
     c++
 }
+*/
+
+// Repetição com Teste Lógico no final
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6) 
