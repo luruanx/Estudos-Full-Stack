c5582f257bfa80309e064bdb5cec453a854ad67f
Ruan Richard <Richard_10luan@hotmail.com>
2024-05-18 01:44:21 -0300

    projeto uni

---
+++ HTML-CSS/Modulo 4/Projeto Port UNI/assets/Styles/script.js
@@ -0,0 +1,39 @@
+
+/* HEADER / NAV */
+
+var header = document.getElementById('header');
+var navigationheader = document.getElementById('navigation-header');
+var content = document.getElementById('content');
+var showSidebar = false;
+
+function toggleSidebar()
+{
+    showSidebar = !showSidebar;
+    if(showSidebar)
+    {
+        navigationheader.style.marginLeft = '-10vw';
+        navigationheader.style.animationName = 'showSidebar';
+        content.style.filter = 'blur(2px)';
+    }
+    else
+    {
+        navigationheader.style.marginLeft = '-100vw';
+        navigationheader.style.animationName = '';
+        content.style.filter = '';
+    }
+}
+
+function closedSidebar()
+{
+    if(showSidebar)
+    {
+        toggleSidebar();
+    }
+}
+
+window.addEventListener('resize', function(event) {
+    if(window.innerWidth > 768 && showSidebar)
+    {
+        toggleSidebar();
+    }
+})
