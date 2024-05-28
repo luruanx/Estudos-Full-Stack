d90b1989fca5d6523854bfe31a96bc3e325b41c2
Ruan Richard <Richard_10luan@hotmail.com>
2024-05-27 23:22:18 -0300

    depur

--- JS/exe002.js
+++ JS/exe002.js
@@ -8,7 +8,14 @@ while (c <= 5) {
 
 // Estrutura FOR
 
+/*
 for(var c = 1; c <= 5; c++) {
     console.log(c)
 }
+*/
 
+console.log('Vai Começar...')
+for(var c = 1; c <= 4; c++) {
+    console.log(c)
+}
+console.log('FIM!')
