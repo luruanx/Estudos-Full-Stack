8e030b849bd3c829facd514ff887179cf31321c4
Ruan Richard <Richard_10luan@hotmail.com>
2024-05-31 23:01:07 -0300

    up

--- JS/Modulo F/ambiente.js
+++ JS/Modulo F/ambiente.js
@@ -1,15 +1,15 @@
 let num = [5, 8, 2, 9, 3]
+num.push(0, 1, 4, 6, 7)
 num.sort()
-num.push(1)
 
 console.log()
 console.log(`Nosso Vetor é o ${num}`)
 console.log(`O vetor tem ${num.length} posições`)
 console.log(`O primeiro valor do vetor é ${num[0]}`)
 
-let pos = num.indexOf(8)
+let pos = num.indexOf(5)
 if (pos == -1) {
     console.log(`O valor não foi encontrado!`)
 } else {
-    console.log(`O valor está na posição ${pos}`)
+    console.log(`O valor procurado está na posição ${pos}`)
 }
