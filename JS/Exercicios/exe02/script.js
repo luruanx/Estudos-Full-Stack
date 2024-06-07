6ecc7011e800fe19b39cee2aa3e267ed59e5868a
Ruan Richard <Richard_10luan@hotmail.com>
2024-06-06 23:49:22 -0300

    exe 02

--- JS/Exercicios/exe02/script.js
+++ JS/Exercicios/exe02/script.js
@@ -9,12 +9,35 @@ function verificar() {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
-        var img = 
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+
         if (fsex[0].checked) {
             gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+                img.setAttribute('src', 'baby M.jpg')
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
         } else if (fsex[1].checked) {
             gênero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                // Criança
+            } else if (idade < 21) {
+                // Jovem
+            } else if (idade < 50) {
+                // Adulto
+            } else {
+                // Idoso
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
