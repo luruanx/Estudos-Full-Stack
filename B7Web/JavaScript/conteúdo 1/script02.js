117b9246d8d635abc0bd06d93b5a58d396500060
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-14 17:32:48 -0300

    template string

---
+++ B7Web/JavaScript/conteúdo 1/script02.js
@@ -0,0 +1,8 @@
+let nome = "Luruanx"
+let sobrenome = "Richard"
+//let nomeCompleto = nome + ' ' + sobrenome;
+
+// OPÇÃO MELHOR: TEMPLATE STRING
+let nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log("Nome: " + nomeCompleto);
