e2bc596e100a131d0cbe8f1b511b32135f41ece4
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-14 16:27:07 -0300

    iniciais

---
+++ B7Web/JavaScript/conteudo 1/script.js
@@ -0,0 +1,8 @@
+let nome = "Luruanx" // string
+let idade = 90 // number
+let logado = true // boolean
+let lista = ["ovo", "farinha", "açucar"] // array
+let nomeCompleto = { nome: "Luruanx", idade: 90 } // object
+
+// o comando abaixo exibe os dados no console
+console.log(typeof nomeCompleto)
