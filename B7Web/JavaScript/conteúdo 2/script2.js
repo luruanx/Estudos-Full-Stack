2904238e9376adcc3715552ebcf15af5f7f2653c
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-19 17:23:17 -0300

    array

--- B7Web/JavaScript/conteúdo 2/script2.js
+++ B7Web/JavaScript/conteúdo 2/script2.js
@@ -9,3 +9,21 @@ const somar = (x, y) => x + y;
 
 console.log(somar(10, 5));
 
+
+//    INTRODUÇÃO A ARRAY     //
+
+//vários valores dentro de uma váriavel
+let colors = ['blue', 'red', 'green'];
+
+let lista = ['ovo', 5, 'farinha', true];
+
+//colocar váriavel e logo após numero do valor
+console.log(colors[1]);
+
+//array dentro de outro array
+let lista2 = [
+    'blabla',
+    ['xx', 'yy']
+];
+
+console.log(lista[1][1])
