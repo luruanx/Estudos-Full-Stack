aaff312c332dac958aba3e9feaa56c9bee8c23d0
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-22 23:45:37 -0300

    loop

---
+++ B7Web/JavaScript/conteúdo 2/loop.js
@@ -0,0 +1,23 @@
+// LOOP
+for (let n = 0; n < 10; n++) {
+    console.log('Frase Qualquer ' + n);
+}
+
+// LOOP EM ARRAY
+let cores = ['preto', 'branco', 'verde', 'azul'];
+cores.push('rosa');
+
+// opção 1
+for (let n = 0; n < cores.length; n++) {
+    console.log(cores[n]);
+}
+
+// opção 2
+for (let i in cores) {
+    console.log(cores[i]);
+}
+
+// opção 3
+for (let cor of cores) {
+    console.log(cor);
+}
