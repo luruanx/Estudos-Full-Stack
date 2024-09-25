e65d1e1df424297cdaee7a48263829a260fa9ae3
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-24 23:46:23 -0300

    array

--- B7Web/JavaScript/conteúdo 2/array.js
+++ B7Web/JavaScript/conteúdo 2/array.js
@@ -17,4 +17,22 @@ ingredientes.shift();
 //remover o último item
 ingredientes.pop();
 
+//retorna uma string, junta e adiciona espaço
+ingredientes.join('-');
+
+//alterar item especifico
+ingredientes[3] = 'sabor';
+ingredientes[ingredientes.length -1] = 'açucar';
+
 console.log(`Total de ingredientes: ${ingredientes.length}`);
+
+
+
+// ORDENAÇÃO DE ARRAY
+let fruits = ['Maça', 'Uva', 'Laranja', 'Banana'];
+
+//em ordem alfabética
+fruits.sort();
+
+//inverte a ordem
+fruits.reverse();
