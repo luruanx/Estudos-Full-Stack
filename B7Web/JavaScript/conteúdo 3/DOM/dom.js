dc620572dd220ec6eb426c7238c48d4e398ae813
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-27 22:48:42 -0300

    dom seletores

---
+++ B7Web/JavaScript/conteúdo 3/DOM/dom.js
@@ -0,0 +1,11 @@
+// DOM
+
+// SELECIONANDO ELEMENTOS
+document.getElementById("--")
+document.getElementsByClassName
+document.getElementsByName
+document.getElementsByTagName
+
+document.querySelector("--")  //return element
+document.querySelectorAll     //return array
+
