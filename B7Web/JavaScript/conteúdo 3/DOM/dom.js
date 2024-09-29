7a35cf01bd7494b9b5a3c6a0333eba388f718aeb
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-29 18:38:06 -0300

    manipulando element

--- B7Web/JavaScript/conteúdo 3/DOM/dom.js
+++ B7Web/JavaScript/conteúdo 3/DOM/dom.js
@@ -1,6 +1,6 @@
 // DOM
 
-// SELECIONANDO ELEMENTOS
+// ---------SELECIONANDO ELEMENTOS
 document.getElementById("--")
 document.getElementsByClassName
 document.getElementsByName
@@ -11,15 +11,25 @@ document.querySelectorAll     //return array
 
 
 
-// EVENTOS DE CLIQUE
+// ----------EVENTOS DE CLIQUE
 //com o onclick
-function cliclou() {
+function clicou() {
     console.log("Clicou no botão");
 }
 
 //direto no JS
 let botao = document.querySelector('.saiba');
 botao.addEventListener("click", () => {
-    cliclou();
+    clicou();
 });
+
+
+// -----------MANIPULAÇAO ELEMENTOS
+// alterando element
+function clickMani() {
+    const teste = document.querySelector('#teste');
+    const element = teste.querySelector('ul');
+
+    element.innerHTML += "<li>Item adicionado</li>"
+}
 
