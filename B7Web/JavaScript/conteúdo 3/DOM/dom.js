c93c10291b87babacfeac1fa77c6bac46cdb7fe4
Ruan Richard <Richard_10luan@hotmail.com>
2024-09-28 23:46:00 -0300

    eventos de clique

--- B7Web/JavaScript/conteúdo 3/DOM/dom.js
+++ B7Web/JavaScript/conteúdo 3/DOM/dom.js
@@ -9,3 +9,17 @@ document.getElementsByTagName
 document.querySelector("--")  //return element
 document.querySelectorAll     //return array
 
+
+
+// EVENTOS DE CLIQUE
+//com o onclick
+function cliclou() {
+    console.log("Clicou no botão");
+}
+
+//direto no JS
+let botao = document.querySelector('.saiba');
+botao.addEventListener("click", () => {
+    cliclou();
+});
+
