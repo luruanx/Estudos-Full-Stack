17946dcb8c537dffa9bc531cd18b6e27b0db2cd6
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-03 22:24:25 -0300

    eventos de teclado

--- B7Web/JavaScript/conteúdo 3/DOM/manicss.js
+++ B7Web/JavaScript/conteúdo 3/DOM/manicss.js
@@ -1,8 +1,31 @@
-// MANIPULANDO CSS
+// MANIPULANDO CSS e CLASS
 
 function clicou() {
     const li = document.querySelector('li');
+    const button = document.querySelector('button');
 
     li.style.backgroundColor = "red";
     li.style.color = 'white'
+
+    button.classList.toggle('botãoone');
 }
+
+
+
+// EVENTOS DE TECLADO
+
+function apertou() {
+    console.log('apertou!')
+}
+
+function segurou() {
+
+}
+
+function soltou(e) {
+    console.log(e.code);
+    console.log('Shift? ' + e.shiftKey);
+}
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', soltou);
