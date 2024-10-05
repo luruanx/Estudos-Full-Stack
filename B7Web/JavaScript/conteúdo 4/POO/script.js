33a64cd82977203ca69813fc1012e4509fa99cb0
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-04 23:02:37 -0300

    Construtor e This

---
+++ B7Web/JavaScript/conteúdo 4/POO/script.js
@@ -0,0 +1,15 @@
+/*
+- Programação Orientada a Objetos (POO - OOP)
+- Programação Procedural
+- Programação Funcional (PF - FP)
+
+- CLASSES
+- FUNÇÕES/OBJETOS
+*/
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
