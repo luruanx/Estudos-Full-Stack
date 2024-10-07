070eb7b6e36b4bce3545a52c6e1dca9a6f7200b7
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-07 18:49:03 -0300

    action

--- B7Web/JavaScript/conteúdo 4/POO/script.js
+++ B7Web/JavaScript/conteúdo 4/POO/script.js
@@ -8,8 +8,31 @@
 */
 
 class Person {
-    constructor(name, age) {
+
+    age = 0;
+    steps = 0;
+
+    constructor(name) {
         this.name = name;
-        this.age = age;
+    }
+
+    //action
+    takeAStep() {
+        this.steps++;
+    }
+
+    setAge(newAge) {
+        if (typeof newAge == 'number') {
+            this.age = newAge;
+        } else {
+            console.log('Apenas Números.')
+        }
     }
 }
+
+// CLASSES: INSTÂNCIA
+let p1 = new Person("João");
+let p2 = new Person("Maria");
+let p3 = new Person("Pedro");
+
+p1.setAge(20);
