f435f4fa8e694b71fab37a07465b959833c9afd4
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-07 22:29:57 -0300

    classes

--- B7Web/JavaScript/conteúdo 4/POO/script.js
+++ B7Web/JavaScript/conteúdo 4/POO/script.js
@@ -44,3 +44,78 @@ let p3 = new Person("Pedro");
 
 p1.age = 20;
 console.log(`${p1.fullName} tem ${p1.age} anos.`)
+
+
+
+// CLASSES: HERANÇA
+class Person1 {
+
+    age = 0;
+
+    constructor(name) {
+        this.name = name;
+    }
+
+    sayHi() {
+        console.log(`${this.name} Olá!`)
+    }
+}
+
+class Student extends Person1 {
+
+    constructor(name, id) {
+        super(name);
+        this.id = id;
+    }
+
+    sayHello() {
+        super.sayHi();
+    }
+}
+
+let p1 = new Student("Luruan", 26);
+p1.age = 20;
+
+p1.sayHello();
+
+
+
+// Classes: Variável/Método estático
+class Person2 {
+
+    static hands = 2;
+    age = 0;
+
+    constructor(name) {
+        this.name = name; 
+    }
+
+    sayHi() {
+        console.log('Oi!')
+    }
+}
+
+let p1 = new Person("Bonieky");
+
+
+
+// Classes: Factory
+class Person3 {
+
+    static hands = 2;
+    age = 0;
+
+    constructor(name) {
+        this.name = name; 
+    }
+
+}
+
+function createPerson(name, age) {
+    let p = new Person3(name);
+    p.age = age;
+    return p;
+}
+
+let p1 = createPerson('Ruan', 90);
+console.log(`${p1.name} tem ${p1.age} anos.`);
