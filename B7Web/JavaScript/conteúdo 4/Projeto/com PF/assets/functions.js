2aeda13b1688c534f1423403dee9ac89adf1efc8
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-09 23:47:59 -0300

    cenário

--- B7Web/JavaScript/conteúdo 4/Projeto/com PF/assets/functions.js
+++ B7Web/JavaScript/conteúdo 4/Projeto/com PF/assets/functions.js
@@ -52,3 +52,39 @@ const createBigMonster = () => {
         defense: 6
     }
 }
+
+// Cenário
+
+const stage = {
+    fighter1: null,
+    fighter2: null,
+    fighter1El: null,
+    fighter2El: null,
+
+    start(fighter1, fighter2, fighter1El, fighter2El) {
+        this.fighter1 = fighter1;
+        this.fighter2 = fighter2;
+        this.fighter1El = fighter1El;
+        this.fighter2El = fighter2El;
+
+        this.fighter1El.querySelector('.attack').addEventListener('click', () => this.doAttack(this.fighter1, this.fighter2));
+        this.fighter2El.querySelector('.attack').addEventListener('click', () => this.doAttack(this.fighter2, this.fighter1));
+
+        this.update();
+    },
+
+    update() {
+        // Fighter1
+        this.fighter1El.querySelector('.name').innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(2)} HP`
+        let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
+        this.fighter1El.querySelector('.bar').style.width = `${f1Pct}%`
+
+        // Fighter2
+        this.fighter2El.querySelector('.name').innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(2)} HP`
+        let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
+        this.fighter2El.querySelector('.bar').style.width = `${f2Pct}%`
+    },
+    doAttack(attacking, attacked) {
+        this.update();
+    }
+}
--- B7Web/JavaScript/conteúdo 4/Projeto/com PF/assets/script.js
+++ B7Web/JavaScript/conteúdo 4/Projeto/com PF/assets/script.js
@@ -1,3 +1,9 @@
 const char = createKnight('Ruan');
 const monster = createLittleMonster();
 
+stage.start(
+    char,
+    monster,
+    document.querySelector('#char');
+    document.querySelector('#monster');
+)
