edf63607b99bc091bd302017ca579de943f983bd
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-27 18:09:57 -0300

    slice and substr

---
+++ B7Web/JavaScript/conteúdo 6/string and num/script.js
@@ -0,0 +1,8 @@
+let nome = 'Ruan Richard';
+
+// tamanho = .length
+// procurar = .indexOf()
+// extrair = .slice(0, ...) ou quantidade = substr(0, ?)
+let resultado = nome.length;
+
+console.log(resultado);
