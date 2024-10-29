aca6672def52ce6d657e4bd7f28e92089bae018c
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-29 01:12:50 -0300

    split

--- B7Web/JavaScript/conteúdo 6/string and num/script.js
+++ B7Web/JavaScript/conteúdo 6/string and num/script.js
@@ -1,8 +1,13 @@
 let nome = 'Ruan Richard';
 
+// divide em array = .split('...')
 // tamanho = .length
 // procurar = .indexOf()
 // extrair = .slice(0, ...) ou quantidade = substr(0, ?)
+// substituir = .replace('nome1', 'nome2)
+// maiúscula = .toUpperCaser()
+// remove esp = .trim()
+
 let resultado = nome.length;
 
 console.log(resultado);
