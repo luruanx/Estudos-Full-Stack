2201405bf297d66ccf7a22c7f2a40b4abd45dc2c
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-29 06:02:05 -0300

    interval and timeout

--- B7Web/JavaScript/conteúdo 6/date and inter/script.js
+++ B7Web/JavaScript/conteúdo 6/date and inter/script.js
@@ -12,3 +12,30 @@ let novoValor = d;
 console.log(novoValor);
 
 
+
+
+// INTERVALOS
+
+let timer;
+
+function comecar() {
+    timer = setInterval(showTime, 1000);
+}
+function parar() {
+    clearInterval(timer);
+}
+
+function showTime() {
+    let d = new Date();
+    let h = d.getHours();
+    let m = d.getMinutes();
+    let s = d.getSeconds();
+    let txt = h + ':' + m + ':' + s;
+
+    document.querySelector('.demo').innerHTML = txt;
+}
+
+// TIMEOUT - CLEARTIMEOUT
+setTimeout(function() {
+    alert('Rodou!')
+}, 5000);
