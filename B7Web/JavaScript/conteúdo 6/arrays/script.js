d79a094cae21defeffdbaaaddc12cdc12fe1c0f5
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-29 02:29:36 -0300

    métodos arrays

---
+++ B7Web/JavaScript/conteúdo 6/arrays/script.js
@@ -0,0 +1,28 @@
+
+// MANIPULAÇÃO DE STRING
+let nome = 'Ruan Richard';
+
+// divide em array = .split('...')
+// tamanho = .length
+// procurar = .indexOf()
+// extrair = .slice(0, ...) ou quantidade = substr(0, ?)
+// substituir = .replace('nome1', 'nome2)
+// maiúscula = .toUpperCaser()
+// remove esp = .trim()
+
+let resultado = nome.length;
+console.log(resultado);
+
+
+
+
+// MÉTODOS NUMBERS
+
+let n = 10;
+
+// trans. string = .toString()
+// qtd. numbers = .toFixed(2);
+// trans. number = .parseInt(n); ou .parseFloat(n);
+
+let res = n.toString();
+console.log(res);
