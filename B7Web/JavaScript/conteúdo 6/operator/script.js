aa0391c74524e7b943edafa2a8da22397b4600b7
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-29 22:44:33 -0300

    spread

---
+++ B7Web/JavaScript/conteúdo 6/operator/script.js
@@ -0,0 +1,32 @@
+// OPERADOR SPREAD
+
+// with array
+let numeros = [1, 2, 3, 4]
+let outros = [...numeros, 5, 6, 7, 8]
+
+// with objects
+let info = {
+    nome: 'Ruan',
+    sobrenome: 'Richard',
+    idade: 26
+};
+let novaInfo = {
+    ...info,
+    cidade: 'Campina Grande',
+    estado: 'Paraíba'
+};
+
+// in function
+
+function adicionarInfo(info) {
+    let novasInfo = {
+        ...info,
+        status: 'ativo',
+        idade: 26,
+        cidade: 'CG'
+    }
+
+    return novasInfo;
+};
+
+console.log(adicionarInfo({nome: 'Ruan', sobrenome: 'Richard'}));
