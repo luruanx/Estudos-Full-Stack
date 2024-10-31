ed040c22b2748f71e40a7dca265203284bbf2dfc
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-31 02:05:56 -0300

    padEnd and padStart

---
+++ B7Web/JavaScript/conteúdo 6/Key and pad/script.js
@@ -0,0 +1,15 @@
+// OBJECT WITH KEY, VALUES, ENTRIES
+
+let lista = {galinha:'ovo', vaca:'carne', fruta:'uva'};
+
+// key, values and entries
+console.log(Object.keys(lista)); 
+
+
+
+// STRING PADSTAR AND PADEND
+
+let telefone = '98654';
+
+console.log(telefone.padEnd(9, '*'));
+console.log(telefone.padStart(9, '*'));
