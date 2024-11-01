db24350d68ec55ec0654e510952aeab1f1ad7b59
Ruan Richard <Richard_10luan@hotmail.com>
2024-10-31 21:22:26 -0300

    promisses

--- B7Web/JavaScript/conteúdo 7/script.js
+++ B7Web/JavaScript/conteúdo 7/script.js
@@ -27,3 +27,32 @@ function alertar() {
 }
 
 setTimeout(alertar, 2000);
+
+
+// PROMISSES - 
+function pegarTemp() {
+    return new Promise(function(resolve, reject) {
+        console.log("pegando temperatura...");
+
+        setTimeout(function(){
+            resolve('40 na sombra');
+        }, 2000);
+    });
+}
+
+// usando a promisse
+console.log("código antes!");
+
+let temp = pegarTemp();
+
+console.log("código durante!");
+
+temp.then(function(resultado) {
+    console.log("TEMPERATURA: "+resultado);
+});
+
+temp.catch(function(error){
+    console.log("Eita, deu erro!");
+});
+
+console.log("código depois!");
