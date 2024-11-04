505d19740accb81981f45b7e4dd26fca934f8837
Ruan Richard <Richard_10luan@hotmail.com>
2024-11-03 23:40:44 -0300

    thumb 2

--- B7Web/JavaScript/conteúdo 7/upthumb.js
+++ B7Web/JavaScript/conteúdo 7/upthumb.js
@@ -30,3 +30,19 @@ function show() {
     document.getElementById("area").appendChild(img);
 }
 
+// opção 2
+
+function mostrar() {
+    let reader = new FileReader();
+    let image = document.getElementById('image').files[0];
+
+    reader.onloadend = function() {
+        let img = document.createElement('img');
+        img.src = reader.result;
+        img.width = 200;
+
+        document.getElementById('area').appendChild(img);
+    }
+
+    reader.readAsDataURL(image);
+}
