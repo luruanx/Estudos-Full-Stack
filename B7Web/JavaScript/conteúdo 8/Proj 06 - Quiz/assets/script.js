6e26c384488f754e799b4837089a6002ed9f4930
Ruan Richard <Richard_10luan@hotmail.com>
2024-11-12 08:07:57 -0300

    finished

--- B7Web/JavaScript/conteúdo 8/Proj 06 - Quiz/assets/script.js
+++ B7Web/JavaScript/conteúdo 8/Proj 06 - Quiz/assets/script.js
@@ -1,12 +1,18 @@
 // Initial Data
 let currentQuestion = 0;
-
+let correctAnswers = 0;
 showQuestion();
+
+// Events
+document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
 
 // Functions
 function showQuestion() {
     if (questions[currentQuestion]) {
         let q = questions[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
 
         document.querySelector('.scoreArea').style.display = 'none';
         document.querySelector('.questionArea').style.display = 'block';
@@ -24,16 +30,45 @@ function showQuestion() {
         });
 
     } else {
-
+        finishQuiz();
     }
 }
 
 function optionClickEvent(e) {
     let cickedOption = parseInt(e.target.getAttribute('data-op'));
 
-    if (questins[currentQuestion].answer === cickedOption) {
-        console.log("errii")
-    } else {
-        console.log("err")
+    if (questions[currentQuestion].answer === cickedOption) {
+        correctAnswers++;
     }
-} 
+
+    currentQuestion++;
+    showQuestion();
+}
+
+function finishQuiz() {
+    let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if (points < 30) {
+        document.querySelector('.scoreText1').innerHTML = 'Ta ruim em?!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    } else if (points >= 30 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom!';
+        document.querySelector('.scorePct').style.color = '#FFFF00';
+    } else if (points >= 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns!!';
+        document.querySelector('.scorePct').style.color = '#51d251';
+    }
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}`;
+
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+}
+
+function resetEvent() {
+    correctAnswers = 0;
+    currentQuestion = 0;
+    showQuestion();
+}
