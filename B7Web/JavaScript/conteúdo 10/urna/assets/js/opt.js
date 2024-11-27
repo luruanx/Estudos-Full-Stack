da39ce34a3cfb67171ad8a7113b89e2d652cbeb6
Ruan Richard <Richard_10luan@hotmail.com>
2024-11-26 23:14:29 -0300

    opt ok

---
+++ B7Web/JavaScript/conteúdo 10/urna/assets/js/opt.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: 38111,
+                nome: 'Fulano',
+                partido: 'PPP',
+                fotos: [
+                    { url: 'homem1.png', legenda: 'Vereador', small: false }
+                ]
+            },
+
+            {
+                numero: 77222,
+                nome: 'Beltrano',
+                partido: 'PTB',
+                fotos: [
+                    { url: 'mulher1.png', legenda: 'Vereador' }
+                ]
+            }
+        ],
+
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: 99,
+                nome: 'Ciclano',
+                partido: 'ABB',
+                vice: 'Zezim',
+                fotos: [
+                    { url: 'homem2.png', legenda: 'Vereador' },
+                    { url: 'homem2.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+
+            {
+                numero: 77,
+                nome: 'Juliao',
+                partido: 'PTB',
+                vice: 'Carlão',
+                fotos: [
+                    { url: 'mulher2.png', legenda: 'Vereador', small: false },
+                    { url: 'mulher2.png', legenda: 'Vice-Prefeito', small: true }
+                ]
+            }
+        ],
+    }
+]
---
+++ B7Web/JavaScript/conteúdo 10/urna/assets/js/script.js
@@ -0,0 +1,60 @@
+let seuVotoPara = document.querySelector('.d-1-1 span');
+let cargo = document.querySelector('.d-1-2 span');
+let descricao = document.querySelector('.d-1-4');
+let aviso = document.querySelector('.d-2')
+let lateral = document.querySelector('.d-1-right');
+let numeros = document.querySelector('.d-1-3');
+
+let etapaAtual = 0;
+let numero = '';
+
+function playEtapa() {
+    let etapa = etapas[etapaAtual];
+    let numeroHtml = '';
+
+    for (let i = 0; i < etapa.numeros; i++) {
+        if (i === 0) {
+            numeroHtml += '<div class="numero pisca"></div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
+    }
+
+    seuVotoPara.style.display = 'none'
+    cargo.innerHTML = etapa.titulo;
+    descricao.innerHTML = '';
+    aviso.style.display = 'none';
+    lateral.innerHTML = '';
+    numeros.innerHTML = numeroHtml;
+};
+
+function f5Front() {
+    
+}
+
+function clicou(n) {
+    let elNumero = document.querySelector('.numero.pisca');
+    if (elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca');
+        if ( elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca');
+        } else {
+            f5Front();
+        }
+            
+    };
+}
+function branco() {
+    alert("clicou");
+}
+function corrige() {
+    alert("clicou");
+}
+function confirma() {
+    alert("clicou");
+}
+
+playEtapa();
