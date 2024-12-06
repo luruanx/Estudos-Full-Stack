220d1c1590ae0b288f7575aebb77d254dd0976bf
Ruan Richard <Richard_10luan@hotmail.com>
2024-12-05 23:48:31 -0300

    primeiro comp

---
+++ B7Web/React/Módulo 1/project/proj01/src/app/page.tsx
@@ -0,0 +1,7 @@
+function Page() {
+  return (
+    <h1>Oláa</h1>
+  )
+}
+
+export default Page;
---
+++ B7Web/React/Módulo 2/project01/src/app/page.tsx
@@ -0,0 +1,12 @@
+function Page() {
+  return(
+    <div>
+      <h1 className="font-bold text-2xl">Olá Mundo</h1>
+      <h3>Outro texto</h3>
+    </div>
+  );
+}
+
+
+
+export default Page;
