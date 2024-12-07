0a16c0734295a02f67bd5878d0b847b2c4d2dd8b
Ruan Richard <Richard_10luan@hotmail.com>
2024-12-06 23:42:38 -0300

    exportanto

--- B7Web/React/Módulo 2/project01/src/app/page.tsx
+++ B7Web/React/Módulo 2/project01/src/app/page.tsx
@@ -1,12 +1,14 @@
-function Page() {
-  return(
+import {Square} from '@/components/square'
+
+const Page = () => {
+  return (
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
       <h3>Outro texto</h3>
+
+      <Square/>
     </div>
   );
 }
-
-
 
 export default Page;
