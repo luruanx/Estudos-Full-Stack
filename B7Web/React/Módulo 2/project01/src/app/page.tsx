0c4f0d718c04e61ff268c54bd072dfe110d535e0
Ruan Richard <Richard_10luan@hotmail.com>
2024-12-13 10:50:51 -0300

    renderizando listas

--- B7Web/React/Módulo 2/project01/src/app/page.tsx
+++ B7Web/React/Módulo 2/project01/src/app/page.tsx
@@ -1,31 +1,55 @@
-import { Person } from '@/components/person'
-import { Card } from '@/components/Card'
+// import { Person } from '@/components/person'
+// import { Card } from '@/components/Card'
+
+// const Page = () => {
+//   return (
+//     <div>
+//       <h1 className="font-bold text-2xl">Olá Mundo</h1>
+//       <h3>Outro texto</h3>
+
+//       {/* <Person 
+//         name='Elon Musk'
+//         avatar='https://files.sunoresearch.com.br/p/uploads/2018/09/Elon-Musk-2-400x300.jpg'
+//         roles={['CEO da Tesla', 'CEO SpaceX']}
+//       />
+//       <Person 
+//         name='Jeff Bezos'
+//         avatar='https://www.infomoney.com.br/wp-content/uploads/2019/06/jeff-bezos-1.jpg?fit=900%2C600&quality=50&strip=all'
+//         roles={['CEO da Amazon', 'CEO BlueOrigin']}
+//       />
+//       <Person 
+//         name='João'
+//         roles={['CEO']}
+//       /> */}
+
+
+//       <Card phrase="Alguma frase top" author='Coach de milhões' />
+
+
+//     </div>
+//   )
+// }
+
+// export default Page;
+
+
+
+import { Person } from "@/components/person";
+import { peopleList } from "@/data/peopleList";
 
 const Page = () => {
+
   return (
     <div>
       <h1 className="font-bold text-2xl">Olá Mundo</h1>
-      <h3>Outro texto</h3>
+      <h3>Outro texto</h3> <br />
 
-      {/* <Person 
-        name='Elon Musk'
-        avatar='https://files.sunoresearch.com.br/p/uploads/2018/09/Elon-Musk-2-400x300.jpg'
-        roles={['CEO da Tesla', 'CEO SpaceX']}
-      />
-      <Person 
-        name='Jeff Bezos'
-        avatar='https://www.infomoney.com.br/wp-content/uploads/2019/06/jeff-bezos-1.jpg?fit=900%2C600&quality=50&strip=all'
-        roles={['CEO da Amazon', 'CEO BlueOrigin']}
-      />
-      <Person 
-        name='João'
-        roles={['CEO']}
-      /> */}
+      <ul>
+        {peopleList.map(person => 
+          <li key={person.id}>{person.name} - {person.profession}</li>
+        )}
+      </ul>
 
-
-      <Card phrase="Alguma frase top" author='Coach de milhões' />
-
-      
     </div>
   )
 }
---
+++ B7Web/React/Módulo 2/project01/src/data/peopleList.ts
@@ -0,0 +1,9 @@
+import { Person } from "@/types/Person"
+
+export const peopleList: Person[] = [
+    { id: 1, name: 'Fulano', profession: 'mathematician' },
+    { id: 2, name: 'Ciclano', profession: 'chemist' },
+    { id: 3, name: 'Beltran', profession: 'physicist' },
+    { id: 4, name: 'Glutan', profession: 'chemist' },
+    { id: 5, name: 'Junglan', profession: 'astrophysicist' }
+]
