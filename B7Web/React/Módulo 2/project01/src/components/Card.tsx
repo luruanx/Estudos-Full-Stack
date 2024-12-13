57bbacb2861e967e581f5a2ec639749fcb090bec
Ruan Richard <Richard_10luan@hotmail.com>
2024-12-12 23:38:53 -0300

    operador &&

--- B7Web/React/Módulo 2/project01/src/components/Card.tsx
+++ B7Web/React/Módulo 2/project01/src/components/Card.tsx
@@ -8,6 +8,8 @@ export const Card = ({ phrase, author }: Props) => {
             <div className="border-2 border-red-600 p3 text-center text-2xl">
                 <h3 className="text-3xl font-bold">{phrase}</h3>
                 <p>- {author ?? 'Autor Desconhecido'}</p>
+                {author && "Tem autor"}
+                {!author && "Não tem autor"}
             </div>
         );
 }
