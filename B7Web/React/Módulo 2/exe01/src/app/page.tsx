48d654ef0d2a4037aae59e2b6b86f0a2a78f234f
Ruan Richard <Richard_10luan@hotmail.com>
2024-12-16 20:27:56 -0300

    condicionais

--- B7Web/React/Módulo 2/exe01/src/app/page.tsx
+++ B7Web/React/Módulo 2/exe01/src/app/page.tsx
@@ -7,18 +7,14 @@ const Page = () => {
   const hour = new Date().getHours();
   let greenting = '';
 
-  if (hour <= 0 && hour < 12) {
-    greenting = 'Bom dia 😊'
-  } else if (hour >= 12 && hour < 12) {
-    greenting = 'Boa tarde 😎'
-  } else if (hour >= 18 && hour <= 23) {
-    greenting = 'Boa noite 🌚'
-  }
-
   return (
     <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
-      <div className="text-5xl font-bold">{greenting}</div>
+      <div className="text-5xl font-bold">
+        {hour >= 0 && hour < 11 && "Bom Dia 😊"}
+        {hour >= 12 && hour < 18 && "Boa Tarde 😎"}
+        {hour >= 18 && hour <= 23 && "Boa Noite 🌚"}
+      </div>
     </div>
   )
 }
