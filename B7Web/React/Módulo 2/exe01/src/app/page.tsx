3661b931702c2b30e4fd62517f9a8da7b342c946
Ruan Richard <Richard_10luan@hotmail.com>
2024-12-16 22:01:18 -0300

    emoji easy

--- B7Web/React/Módulo 2/exe01/src/app/page.tsx
+++ B7Web/React/Módulo 2/exe01/src/app/page.tsx
@@ -1,3 +1,5 @@
+import { EmojiRating } from "@/components/EmojiRating";
+
 const Page = () => {
   const fullTime = new Intl.DateTimeFormat('pt-br', {
     timeStyle: 'short',
@@ -8,12 +10,16 @@ const Page = () => {
   let greenting = '';
 
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-500 to-indigo-500">
+    <div className="w-screen h-screen flex flex-col justify-center items-center text-white bg-gradient-to-r from-sky-900 to-indigo-500">
       <div className="text-9xl">{fullTime}</div>
       <div className="text-5xl font-bold">
         {hour >= 0 && hour < 11 && "Bom Dia 😊"}
         {hour >= 12 && hour < 18 && "Boa Tarde 😎"}
         {hour >= 18 && hour <= 23 && "Boa Noite 🌚"}
+      </div>
+
+      <div>
+        <EmojiRating rate={3.2} />
       </div>
     </div>
   )
---
+++ B7Web/React/Módulo 2/exe01/src/components/EmojiRating.tsx
@@ -0,0 +1,18 @@
+type Props = {
+    rate: number
+}
+
+export const EmojiRating = ({ rate }: Props) => {
+    if (rate > 5) rate = 5;
+    if (rate < 0) rate = 0;
+
+    const rateInt = Math.floor(rate)
+    const stars = '★'.repeat(rateInt) + '☆'.repeat(5 - rateInt)
+
+    return (
+        <div className="m-5 flex items-center text-3xl">
+            <div className="bg-gray-700 p-2 rounded">{rate.toFixed(1)}</div>
+            <div className="ml-3">{stars}</div>
+        </div>
+    )
+}
