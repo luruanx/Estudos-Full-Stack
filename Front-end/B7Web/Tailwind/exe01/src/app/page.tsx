0c4b2eafaf210a50a0d76e6e8256faf2d901ad7b
Ruan Richard <Richard_10luan@hotmail.com>
2025-02-27 01:54:52 -0300

    tailwind loading

---
+++ Front-end/B7Web/Tailwind/exe01/src/app/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => (
+  <div className="container mx-auto">
+
+    <div className="m-8 w-12 h-12 border-8 border-blue-200 border-r-blue-600 rounded-full animate-spin"></div>
+
+  </div>
+
+
+)
+
+export default Page;
