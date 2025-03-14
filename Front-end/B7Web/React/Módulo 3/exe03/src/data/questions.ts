303d72e00b3c028efa0a6f59846cf430c6139117
Ruan Richard <Richard_10luan@hotmail.com>
2025-03-14 01:39:00 -0300

    estrutura1

---
+++ Front-end/B7Web/React/Módulo 3/exe03/src/data/questions.ts
@@ -0,0 +1,14 @@
+import { Question } from "@/types/Question";
+
+export const questions: Question[] = [
+    {
+        question: 'Qual ingrdiente não pode faltar?',
+        options: [
+            'Farinha',
+            'Corante',
+            'Água',
+            'Açucar'
+        ],
+        answer: 1
+    }
+];
