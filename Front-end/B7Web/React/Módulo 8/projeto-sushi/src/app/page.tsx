37c85be9cbad01c064aa86722f2f94c5c85d0cf0
Ruan Richard <Richard_10luan@hotmail.com>
2025-04-10 23:12:17 -0300

    home and footer

--- Front-end/B7Web/React/Módulo 8/projeto-sushi/src/app/page.tsx
+++ Front-end/B7Web/React/Módulo 8/projeto-sushi/src/app/page.tsx
@@ -1,12 +1,18 @@
-import { ThemeToggle } from "@/components/theme-toggle";
-import { Button } from "@/components/ui/button";
+
+import { Footer } from "@/components/footer";
+import { Header } from "@/components/header";
 
 const Page = () => {
     return (
         <div className="w-full max-w-4xl mx-auto">
-            ...
+            <Header />
+            <div className="mx-3">
+                ...
+            </div>
+            <Footer />
         </div>
+        
     )
-    }
+}
 
 export default Page;
