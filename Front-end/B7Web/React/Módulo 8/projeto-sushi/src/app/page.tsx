099229d6c5065976545558bea5dd6f15bd2adf63
Ruan Richard <Richard_10luan@hotmail.com>
2025-04-10 22:56:18 -0300

    page structure

---
+++ Front-end/B7Web/React/Módulo 8/projeto-sushi/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { ThemeToggle } from "@/components/theme-toggle";
+import { Button } from "@/components/ui/button";
+
+const Page = () => {
+    return (
+        <div className="w-full max-w-4xl mx-auto">
+            ...
+        </div>
+    )
+    }
+
+export default Page;
