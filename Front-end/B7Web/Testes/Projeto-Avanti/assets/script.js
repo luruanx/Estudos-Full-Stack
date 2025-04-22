f5315230aeed86741d8abd3cfa796fb7086026e4
Ruan Richard <Richard_10luan@hotmail.com>
2025-04-22 03:02:15 -0300

    carousel product

--- Front-end/B7Web/Testes/Projeto-Avanti/assets/script.js
+++ Front-end/B7Web/Testes/Projeto-Avanti/assets/script.js
@@ -132,3 +132,38 @@ document.addEventListener("mousemove", (e) => {
     menuitemBox.style.display = "none";
   }
 });
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 20,
+    },
+  },
+});
