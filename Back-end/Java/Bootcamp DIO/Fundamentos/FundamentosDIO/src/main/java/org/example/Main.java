b80178cdf73b34ace935c4ebe3f51116d01c994a
Ruan Richard <ruan.richardd@hotmail.com>
2025-11-10 13:14:00 -0300

    keywords e tipos primitivos

---
+++ Back-end/Java/Bootcamp DIO/Fundamentos/FundamentosDIO/src/main/java/org/example/Main.java
@@ -0,0 +1,18 @@
+package org.example;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte ex1 = 1;  //Tamanho: 8 bits
+        short ex2 = 2; //Tamanho: 16 bits
+        int ex3 = 3;   //Tamanho: 32 bits
+        long ex4 = 4L; //Tamanho: 64 bits
+
+        float ex5 = 5.0f;         //Tamanho: 32 bits
+        double ex6 = 60.60d;  //Tamanho: 64 bits
+
+        char character = 'a'; // aspas simples
+        boolean bolleano = true; 
+    }
+}
